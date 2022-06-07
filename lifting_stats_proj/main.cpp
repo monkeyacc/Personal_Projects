56d8d04b225b9d537a3ff34f3d9708cdf92d8291
Miguel Cortez <miguel.cortez1337@gmail.com>
2022-06-07 00:47:06 -0500

    got some things working mate

---
+++ lifting_stats_proj/main.cpp
@@ -0,0 +1,46 @@
+/*
+This program will ask the user whether they would like to enter 
+lifting data and/or would like to generate stats.
+
+Author: Miguel Cortez
+Date: 6/6/2022
+*/
+
+#include <iostream>   // For Input & Output
+#include <string>     // For string manipulation functionalities
+#include <fstream>    // For reading in files
+#include <cassert>    // For the assert statement used to validate file open
+#include <vector>     // For vector functionalities
+#include <sstream>    // For string operations (i.e parsing)
+#include <algorithm>  // For "find" functionalities
+#include <cctype>     // For .toupper()
+
+using namespace std;
+
+class Node; // this is the declaration, so that it can be used inside class liftingData below
+
+class liftingData
+{
+    private:
+        int exerWeight;
+        int exerReps;
+        double exerTimeDuration;
+        double exerDistance;
+        
+
+
+
+
+
+
+};
+
+
+
+int main()
+{
+    
+    
+
+    return 0;
+}
